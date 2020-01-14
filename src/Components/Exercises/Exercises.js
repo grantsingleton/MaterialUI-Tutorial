f581e73e88ab76ce8cfcfe78be262a89eac55968
Grant Singleton <46718281+grantsingleton@users.noreply.github.com>
2020-01-14 11:21:42 -0600

    Finalize Layout

--- src/Components/Exercises/Exercises.js
+++ src/Components/Exercises/Exercises.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import RightPane from './RightPane'
+import LeftPane from './LeftPane'
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    padding: 20,
+    marginTop: 10,
+    marginBottom: 10,
+  },
+}));
 
 export default function Exercises() {
+
+  const classes = useStyles();
+
   return (
-    <div>
-      Hello From Exercises
-    </div>
+    <Grid container spacing={2}>
+      <Grid item sm>
+        <LeftPane classes={classes}/>
+      </Grid>
+      <Grid item sm>
+        <RightPane classes={classes}/>
+      </Grid>
+    </Grid>
   );
 }
---
+++ src/Components/Exercises/LeftPane.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Paper from '@material-ui/core/Paper';
+
+export default function LeftPane( {classes} ) {
+
+  return (
+    <Paper className={classes.paper}>
+     Left Pane
+    </Paper>
+  )
+}
